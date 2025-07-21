94bc832d56d0c00038d215092840f562895c2d36
shali <shali@shalini>
2025-07-21 12:29:11 +0530

    java project

---
+++ C2TC_Shalini/src/Shal/project.java
@@ -0,0 +1,12 @@
+package Shal;
+
+public class project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my Class");
+	
+
+	}
+
+}
