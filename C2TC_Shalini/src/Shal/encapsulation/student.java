16587cb9d808b9175a07768aa6e6316ef548d19d
shali <shali@shalini>
2025-07-21 16:36:04 +0530

    encapsulation

--- C2TC_Shalini/src/Shal/encapsulation/student.java
+++ C2TC_Shalini/src/Shal/encapsulation/student.java
@@ -1,7 +1,30 @@
 package Shal.encapsulation;
 
 public class student {
-	int id;
+	
+	private int id;
 	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String toString()
+	{
+		return"student[id=" + id + ", name= "+ name + "]";
+		//return"Student[id=" +id + ", name="+ name +"]";
+	}
+
 
 }
--- C2TC_Shalini/src/Shal/encapsulation/studentdemo.java
+++ C2TC_Shalini/src/Shal/encapsulation/studentdemo.java
@@ -4,8 +4,20 @@ public class studentdemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student obj;
-		obj=new Student()
+		   student obj = new student();
+		
+			
+			obj.setId(101);
+			obj.setName("Keshu");
+			System.out.println(obj.getId());
+			System.out.println(obj.getName());
+			System.out.println();
+			System.out.println(obj);
+			
+			
+
+		}
+
 
 	}
 
