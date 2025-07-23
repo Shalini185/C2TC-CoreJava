3a63aa980e02de9971a637e3a68e94c83173ea62
shali <shali@shalini>
2025-07-23 21:40:33 +0530

    Changes in constructor

--- C2TC_Shalini/src/day2/entiyl/Customer01.java
+++ C2TC_Shalini/src/day2/entiyl/Customer01.java
@@ -4,15 +4,37 @@ public class Customer01 {
 	private int id;
 	private String name;
 	private String city;
+	
+	//default constructor
 	public Customer01() {
+		System.out.println("");
 		this.id=41;
 		this.name="unknown";
 		this.city="pudhucherry";
 		
 	}
+	
+	 
+	//parameterized constructor with 3parameter
+	public Customer01(int id, String name, String city) {
+		//super();
+		this();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		
+	}
+	//parameterized constructor with 2parameter
+	public Customer01(int id, String name) {
+		//super();
+		this.id = id;
+		this.name = name;
+		//System.out.println(id ":" + name +);
+	}
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
--- C2TC_Shalini/src/day2/entiyl/CustomerDemo.java
+++ C2TC_Shalini/src/day2/entiyl/CustomerDemo.java
@@ -3,15 +3,17 @@ package day2.entiyl;
 public class CustomerDemo {
 	public static  void main(String[] args) {
 		 Customer01 cus1 = new Customer01();
-		 Customer01 cus2 = new Customer01();
-		 Customer01 cus3 = new Customer01();
+		 Customer01 cus2 = new Customer01(41,"shalzz","Chennai");
+		 Customer01 cus3 = new Customer01(42,"ram");
 		
 		 //cus1.setId(41);
 		 cus2.setId(62);
 		// cus1.setName("Shalini");
 		 cus2.setName("Ram");
 		// cus1.setCity("Cuddalore");
-		 System.out.println(cus1);
+		 cus3.setName("Ramkumar");
+		 System.out.println(cus2);
+		 System.out.println(cus3);
 		 System.out.println("Customer 1 city : " + cus1.getCity() + " Customer 1 name : " + cus2.getName() );
 		 
 	}
