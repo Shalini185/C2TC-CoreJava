9cf2a6b9d1d444b8b7f972a50ce64ca300ba88d2
shali <shali@shalini>
2025-07-23 13:32:45 +0530

    printing name, age, serialno as output using encapsulation

---
+++ C2TC_Shalini/src/Day3/Enapsulation1/EncapsulationDemo.java
@@ -0,0 +1,13 @@
+package Day3.Enapsulation1;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		Encapsulation var1=new Encapsulation();
+		
+		var1.setSerialNo(121);
+		var1.setName("Shalu");
+		var1.setAge(20);
+		System.out.println(var1);
+	}
+
+}
