d6b3b0d1e72d1f0f17026765b7446a59d6a85ff2
shali <shali@shalini>
2025-07-28 22:05:37 +0530

    Code using abstract

---
+++ C2TC_Shalini/src/day7/Abstract/PaymentSystem.java
@@ -0,0 +1,23 @@
+package day7.Abstract;
+//abstract class
+abstract class PaymentGateway {
+	public abstract void paymentProcess(double amt);
+	public void connectToPayment() {
+		System.out.println("Connecting to the payment gateway...");
+	}
+}
+class CreditPayment extends PaymentGateway{
+	@Override 
+	public void paymentProcess(double amt) {
+		System.out.println("Processing the amount Rs." + amt );
+	}
+	
+}
+public class PaymentSystem{
+	public static  void main(String[] args) {
+		PaymentGateway payment;
+		payment = new CreditPayment();
+		payment.connectToPayment();
+		payment.paymentProcess(2000);
+	}
+}
